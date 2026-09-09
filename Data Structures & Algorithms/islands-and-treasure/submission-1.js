29063b2da567e19e22b70e96fcfc54201d86ef48
Gaurav Singh <32643752+gsrishu@users.noreply.github.com>
2026-09-09 17:44:05 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     */
+    islandsAndTreasure(grid) {
+        const queue = new Array();
+        const [row, col] = [grid.length, grid[0].length];
+        const visited = new Set();
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                if (grid[i][j] == 0 && !visited.has(`${i}-${j}`)) {
+                    queue.push([i, j]);
+                }
+            }
+        }
+      const direction = [[-1,0],[1,0],[0,1],[0,-1]]
+        while (queue.length) {
+            let [i, j] = queue.shift();
+          for(const [di,dj] of direction){
+             const ni = i + di
+             const nj = j + dj
+
+             const inside = ni >= 0 && ni < row && nj >= 0 && nj < col
+             if(!inside) continue
+
+             if(grid[ni][nj]== 2147483647){
+                grid[ni][nj] = grid[i][j] +1
+                queue.push([ni,nj])
+             }
+          }
+        }
+        return grid;
+    }
+}
