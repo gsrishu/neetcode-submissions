54616cdd253cdfd8a79094a7f19f8c4474c626aa
Gaurav Singh <32643752+gsrishu@users.noreply.github.com>
2026-09-22 19:59:56 +0530

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     */
+    islandsAndTreasure(grid) {
+        const [row, col] = [grid.length, grid[0].length];
+        const visited = new Set();
+        function dfs(i, j, dist) {
+            if(i <  0 || i >= row || j < 0 || j>= col) return
+            if ( grid[i][j] == -1) return;
+            if(dist > grid[i][j]) return
+            grid[i][j] = dist;
+            dfs(i + 1, j, dist + 1);
+            dfs(i - 1, j, dist + 1);
+            dfs(i, j + 1, dist + 1);
+            dfs(i, j - 1, dist + 1);
+        }
+
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                if (grid[i][j] == 0) {
+                    dfs(i, j, 0);
+                }
+            }
+        }
+        return grid;
+    }
+}
