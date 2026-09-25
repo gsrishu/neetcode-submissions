089d6625deb68daedb20634b322a4a86bfa25b4c
Gaurav Singh <32643752+gsrishu@users.noreply.github.com>
2026-09-25 07:33:45 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const inbound = new Array(numCourses).fill(0);
+        const graph = new Array(numCourses).fill(null).map(() => []);
+        for(const [i,j] of prerequisites){
+            inbound[i]++
+            graph[j].push(i)
+        }
+        const queue = new Array()
+        let idx = 0
+
+        for(let i = 0; i< inbound.length;i++){
+            if(inbound[i] == 0) queue.push(i)
+        }
+        const topo = new Array()
+        while(idx < queue.length){
+            const node = queue[idx++]
+            topo.push(node)
+            for(const i of graph[node]){
+                inbound[i]--
+                if(inbound[i] === 0) queue.push(i)
+            }
+
+        }
+        return (topo.length == numCourses) ? true : false
+    }
+}
